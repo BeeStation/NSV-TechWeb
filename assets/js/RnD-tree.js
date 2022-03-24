320a907f2731724dae413c741d9ae08ca346d424
Bobbanz1 <Bobbanz14@gmail.com>
2022-03-24 15:43:32 +0100

    Updated some files
    Added the start on the ability to highlight the requirements for certain nodes...still no idea why it doesn't work on nodes in the same tree

--- assets/js/RnD-tree.js
+++ assets/js/RnD-tree.js
@@ -16,7 +16,7 @@ var configWeb = {
     },
     node: {
         HTMLclass: 'web',
-        collapsable: true
+        collapsable: false
     },
     animation: {
         nodeAnimation: "easeOutBounce",
@@ -27,6 +27,7 @@ var configWeb = {
         onTreeLoaded: function() {
             const observer = lozad();
             observer.observe();
+            badwolf();
         }
     }
 };
@@ -60,6 +61,90 @@ function load_tree() {
         $.getJSON('Techweb.json', function(jsonData) {
             setup(jsonData);
             _load(jsonData);
-        });    
+    });    
 }
 
+
+function reqFetch(string) {
+    //Grabs the json file and throws the resulting data into a local variable
+    $.getJSON('Techweb.json',function(jsonData) {
+        //Variable is sent to a filter function that sorts through it.
+        //Finding any entry that requires multiple parents...sort of
+        mental(jsonData, string);
+    });
+}
+
+
+//The sticky peanut butter of it all
+function badwolf() {
+    //Upon clicking any node
+    $("div.node").click(function() {
+        //The code will grab the node that was clicked and put it in an array
+        var Grabby = $(this).children();
+        var tempary = [];
+        //For every entry in this array, it will push it to another array
+        Grabby.each(function(i, node){
+            tempary.push(node);
+        });
+        //The code then removes three array elements which are un-needed 
+        tempary.splice(1,3);
+        cat = $(tempary).attr("title"); 
+        reqFetch(cat);
+    });        
+}
+
+function Hell(Slapper) {
+    var cera = $("div#Techweb").children();
+    var carl = [];
+    cera.each(function(i, node){
+        carl.push(node);
+    });
+    carl.splice(0,1);
+    chaos(Slapper, carl);
+}
+
+function chaos(Slapper, free) {
+    $(free).each(function(i, node){
+        let fock = $(node).children();
+        $(fock).each(function(i, nodetwo){
+                for (let x = 0; x < Slapper.length; x++) {
+                    var element = Slapper[x];
+                    if ($(nodetwo).attr("title")==element) {
+                        if ($(nodetwo).attr("selected")=="selected") {
+                            $(nodetwo).removeAttr("selected");
+                        } else {
+                            $(nodetwo).attr("selected", "highlight");
+                        }        
+                    }
+                }
+        });
+    });
+}
+
+function mental(jsonData, string) {
+    $(jsonData.children).each(function(i, node){
+        if (!(node.requirement === undefined)) {
+            var Slapper = node.requirement;
+            var Blocker = node.name;
+            if (string == Blocker) {
+                Hell(Slapper);
+                return;
+            }
+        } else {
+            mental(node, string);
+        }    
+    });
+}
+
+let cat;
+
+/*
+        //The code checks if the resulting array's last entry has an attribute with the name selected
+        if ($(tempary).attr("selected")=="selected") {
+            //If it does, then it removes it
+            $(tempary).removeAttr("selected");
+        } else {
+            $(tempary).attr("selected", "you"); 
+        }
+
+*/
